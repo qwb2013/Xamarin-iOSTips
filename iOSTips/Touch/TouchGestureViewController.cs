54b4d33a208fcff1d9e609580c0eac1a0c77da59
Fang Huai An <developer.fang@gmail.com>
2016-10-28 17:08:54 +0800

    touch events

--- iOSTips/Touch/TouchGestureViewController.cs
+++ iOSTips/Touch/TouchGestureViewController.cs
@@ -2,12 +2,14 @@
 
 using UIKit;
 
-using Debug = System.Diagnostics.Debug;
+using static System.Console;
 
 namespace iOSTips
 {
 	public partial class TouchGestureViewController : UIViewController
 	{
+		private double Distance { get; set; }
+
 		public TouchGestureViewController (IntPtr handle) : base (handle)
 		{
 			
@@ -19,10 +21,34 @@ namespace iOSTips
 			// Perform any additional setup after loading the view, typically from a nib.
 			Title = "Touch";
 
-			UITapGestureRecognizer tapGestureRecognizer = new UITapGestureRecognizer (() => {
-				Debug.WriteLine ("Tap !");
+			UITapGestureRecognizer tapGestureRecognizer = new UITapGestureRecognizer ((UITapGestureRecognizer recognizer) => {
+				WriteLine ("Tap !");
 
-				//tapGestureRecognizer.LocationInView( 
+				var locationTop = recognizer.LocationInView (topView);
+				var locationBottom = recognizer.LocationInView (bottomView);
+
+				var isInTop = locationTop.X < topView.Bounds.Width && locationTop.X > 0
+										 && locationTop.Y > 0 && locationTop.Y < topView.Bounds.Height;
+				var isInBottom = locationBottom.X < bottomView.Bounds.Width && locationBottom.X > 0
+										 && locationBottom.Y > 0 && locationBottom.Y < bottomView.Bounds.Height;
+
+				lbMessage.Text = $"Top:{isInTop}:X:{locationTop.X};Y:{locationTop.Y};Bottom:{isInBottom}:X:{locationBottom.X};Y:{locationBottom.Y};";
+
+				if (isInTop) {
+					topView.BackgroundColor = UIColor.FromRGB (0, 128, 255);
+					bottomView.BackgroundColor = UIColor.FromRGB (0, 128, 0);
+				} 
+				else if (isInBottom) {
+					bottomView.BackgroundColor = UIColor.FromRGB (0, 128, 255);
+					topView.BackgroundColor = UIColor.FromRGB (0, 128, 0);
+				} 
+				else { 
+					topView.BackgroundColor = UIColor.FromRGB (255, 255, 0);
+					bottomView.BackgroundColor = UIColor.FromRGB (255, 255, 0);
+				}
+
+
+
 			});
 
 			this.View.AddGestureRecognizer (tapGestureRecognizer);
@@ -40,12 +66,46 @@ namespace iOSTips
 
 		public override void TouchesBegan (Foundation.NSSet touches, UIEvent evt)
 		{
+			WriteLine ($"count:{ touches.Count }");
+
 			base.TouchesBegan (touches, evt);
 		}
 
 		public override void TouchesMoved (Foundation.NSSet touches, UIEvent evt)
 		{
-			base.TouchesMoved (touches, evt);
+			
+			if (2 == touches.Count) {
+				
+
+				var locations = touches.ToArray<UITouch> () ;
+
+				UITouch loc0 = locations [0];
+				var p0 = loc0.LocationInView (this.View);
+
+				UITouch loc1 = locations [1];
+				var p1 = loc1.LocationInView (this.View);
+
+				double distance = Math.Sqrt ( Math.Pow ((p0.X - p1.X), 2.0) + Math.Pow ((p0.Y - p1.Y), 2.0));
+
+				if (Distance > distance) {
+					lbMessage.Text = $"兩指接近; distance:{distance}";
+
+					topView.BackgroundColor = UIColor.FromRGB (255, 0, 0);
+					bottomView.BackgroundColor = UIColor.FromRGB (255, 0, 0);
+				} 
+				else { 
+					lbMessage.Text = $"兩指離開; distance:{distance}";
+
+					topView.BackgroundColor = UIColor.FromRGB (0, 0, 255);
+					bottomView.BackgroundColor = UIColor.FromRGB (0, 0, 255);
+				}
+
+
+
+				Distance = distance;
+
+
+			}
 		}
 
 		public override void TouchesEnded (Foundation.NSSet touches, UIEvent evt)
